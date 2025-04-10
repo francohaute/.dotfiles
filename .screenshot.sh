16a65aaa2c49ef877d9a5b6a434806349fb255c7
Franco Hauteville <francohauteville@gmail.com>
2025-04-10 13:14:29 -0300

    -

---
+++ .screenshot.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# Generate a timestamp
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+# Define the filename
+filename="$timestamp.png"
+
+# Start Flameshot and save the screenshot with the filename
+flameshot gui -p ~/code/codenotes/img/$filename
+
+# Save the filename to the X11 clipboard
+echo -n $filename | xsel --clipboard --input
+
+echo "Screenshot saved as $filename and filename copied to clipboard."
+
