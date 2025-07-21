768fcdc37714b990ece6380d8b250275a1d70b9c
Franco Hauteville <francohauteville@gmail.com>
2025-07-20 23:03:27 -0300

    Screenshot agnostic of protocol

--- .kde_screenshot.sh
+++ .kde_screenshot.sh
@@ -6,6 +6,9 @@ filename="$timestamp.png"
 
 spectacle -r -b -n -o ~/codenotes/img/$filename
 
-echo -n $filename | wl-copy
-
+if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
+    echo -n "$filename" | wl-copy
+elif [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
+    echo -n "$filename" | xsel --clipboard --input
+fi
 echo "Screenshot saved as $filename and filename copied to clipboard."
